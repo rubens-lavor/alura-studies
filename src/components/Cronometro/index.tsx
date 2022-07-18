b014f352e9901cec4a131a3feed2b77a3a8778a5
rubens-lavor <haryson07@gmail.com>
2022-07-17 23:52:38 -0300

    add time with useEffect

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,11 +1,27 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from './Cronometro.module.scss';
+import { ITarefa } from "../../types/tarefa";
+import { useEffect, useState } from "react";
+import { tempoParaSegundos } from "../../common/utils/time";
 
-export default function Cronometro() {
+interface Props {
+  selecionado: ITarefa | undefined
+}
+
+export default function Cronometro({ selecionado }: Props) {
+  const [tempo, setTempo] = useState<number>()
+
+  useEffect(() => {
+    if (selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado.tempo))
+    }
+  }, [selecionado])
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
+      Tempo: {tempo}
       <div className={style.relogioWrapper}>
         <Relogio />
       </div>
